654bb094eeb0e9c2c1407e80786e1cf74675af18
Joon Ki Choi <j.choi@fadendaten.ch>
2016-11-14 18:58:16 +0100

    wip: export values from module that behaves like a container

--- confusions.js
+++ confusions.js
@@ -1,27 +1,28 @@
-// //to illustrate how `this` doesn't let a function get a reference to itself
-// 
-// var Confusions1 = function() {};
-// 
-// Confusions1.prototype.foo = function(num) {
-// 	// keep track of how many times `foo` is called
-// 	this.count++;
-// }
-// 
-// module.exports = Confusions1;
+//to illustrate how `this` doesn't let a function get a reference to itself
+
+var Confusion1 = function() {};
+
+Confusion1.prototype.foo = function(num) {
+	// keep track of how many times `foo` is called
+	this.count++;
+}
+
+module.exports = Confusion1;
 
 
 // to illustrate how `this` doesn't refers to the functions's scope
 
-var Confusions2 = function() {};
+var Confusion2 = function() {};
 
-Confusions2.prototype.foo = function() {
+Confusion2.prototype.foo = function() {
 	var a = 2;
 	this.bar(); // However, the developer who writes such code is attempting to use this to create a bridge between the lexical scopes of foo() and bar(), so that bar() has access to the variable a in the inner scope of foo(). No such bridge is possible. You cannot use a this reference to look something up in a lexical scope. It is not possible.
 };
 
-Confusions2.prototype.bar = function() {
+Confusion2.prototype.bar = function() {
 	return this.a;
 }
 
-module.exports = Confusions2;
+exports = Confusion1;
+exports = Confusion2;
 
--- confusions.spec.js
+++ confusions.spec.js
@@ -1,40 +1,40 @@
-var Confusion = require('./confusions.js');
+var confusions = require('./confusions.js');
 
 describe("chapter 1, common misconception ", function() {
-	xdescribe("to assume `this` refers to the function itself: ", function() {
-		var confusion = new Confusion();
+	describe("to assume `this` refers to the function itself: ", function() {
+		var confusion1 = confusions.Confusion1;
 
-		confusion.foo.count = 0;
+		confusion1.foo.count = 0;
 
 		var failRun = function(num) {
 			for (var i=0; i<num; i++) {
-				confusion.foo(i); // it doesn't work this way
+				confusion1.foo(i); // it doesn't work this way
 				console.log('foo.count: ' + foo.count);
 			}
 		}
 
 		var run = function(num) {
 			for (var i=0; i<num; i++) {
-				confusion.foo.call(confusion.foo, i); // using `call` force `this` to actually point at the foo function object
+				confusion1.foo.call(confusion.foo, i); // using `call` force `this` to actually point at the foo function object
 			};
 		}
 
 		it("it doesn't count how many times foo was called", function() {
-			expect(function(){ failRun(10) }).toThrow( new Error("foo is not defined") );
+			expect(function(){ failRun(10) }).toThrow( new Error("Cannot read property 'foo' of undefined") );
 		});
 
 		it("it does count how many times foo was called", function() {
 			run(5);
-			expect(confusion.foo.count).toEqual(5);
+			expect(confusion1.foo.count).toEqual(5);
 		});
 
 	});
 
 	describe("to assume `this` refers to the function's scope", function() {
-		var confusion = new Confusion();
+		var confusion2 = confusions.Confusion2;
 
 		it("there's no bridge between the lexical scopes of foo() and bar()", function() {
-			expect(confusion.foo).toThrow( new Error("this.bar is not a function") );
+			expect(confusion2.foo).toThrow( new Error("this.bar is not a function") );
 		});
 		//
 	});
