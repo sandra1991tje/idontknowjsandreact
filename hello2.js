1bbcd76d604ae3df92f2256210d735de9501304f
sandra1991tje <sandragerber2@gmail.com>
2016-11-18 15:08:07 +0100

    add new test

---
+++ hello2.js
@@ -0,0 +1,7 @@
+var HelloMyNameIs = function() {};
+
+HelloMyNameIs.prototype.speak = function( context ) {
+  return "Hello, my name is SLIM SHADY";
+};
+
+module.exports = HelloMyNameIs;
---
+++ hello2.spec.js
@@ -0,0 +1,16 @@
+// test for hello.js
+// execute test with: jasmine-node .
+
+var SagWas = require('./hello2.js');
+
+describe("Returns a string  ", function() {
+  var slimShady = new SagWas();
+
+  it('says Hello my name is Slim Shady', function() {
+    expect(slimShady.speak( "SLIM SHADY" )).toEqual('Hello, my name is SLIM SHADY');
+  });
+
+  // test to print speak argument with amount of character as a prefix
+  // eg. slimShady.speak( "SLIM SHADY" ) => "4SLIM 5SHADY"
+  // eg. slimShady.speak( "SLIM SHADY Lorem" ) => "4SLIM 5SHADY 6LOREMI"
+});
