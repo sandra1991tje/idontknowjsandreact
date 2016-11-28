a91e614bd167227204ba8be8dd6583b1b0c846e5
sandra1991tje <sandragerber2@gmail.com>
2016-11-28 16:10:49 +0100

    fixed bug in exercise

--- hello2.js
+++ hello2.js
@@ -3,10 +3,11 @@ var HelloMyNameIs = function() {};
 HelloMyNameIs.prototype.speak = function( context ) {
   var strIn= context.split(" ");
   var strOut = "";
-  for(i=0;i<strIn.length;i++){
+  for(i = 0; i < strIn.length; i++){
     strOut = strOut.concat(strIn[i].length+strIn[i] + " ");
   }
-
+  // remove last line break
+  strOut = strOut.slice(0, - 1);
   
   return strOut.toUpperCase();
 };
